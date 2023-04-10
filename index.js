8948b79fdcf1cb45a730b167e8e15cde32787200
vnvz <vvv@cesar.school>
2023-04-10 00:28:29 -0300

    finalizando index.js, adicionando método para obter contexto de mensagens antigas

--- index.js
+++ index.js
@@ -34,12 +34,21 @@ client.on("messageCreate", async (message) => {
     },
   ];
 
-  conversationLog.push({
-    role: "user",
-    content: message.content,
-  });
-
   await message.channel.sendTyping();
+
+  let prevMessages = await message.channel.messages.fetch({ limit: 15 });
+  prevMessages.reverse();
+
+  prevMessages.forEach((msg) => {
+    if (message.content.startsWith("!")) return;
+    if (msg.author.id !== client.user.id && message.author.bot) return;
+    if (msg.author.id !== message.author.id) return;
+
+    conversationLog.push({
+      role: "user",
+      contet: msg.content,
+    });
+  });
 
   const result = await openai.createChatCompletion({
     model: "gpt-3.5-turbo",
